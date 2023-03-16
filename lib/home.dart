c8cbabf959a93f94d05068a1b71ee1f4d4e7f435
Aldi0705 <aldisayahputra913@gmail.com>
2023-03-16 22:46:06 +0700

    home

--- lib/home.dart
+++ lib/home.dart
@@ -11,10 +11,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const _pages = [
-      Icon(
-        Icons.call,
-        size: 150,
+     final _pages = [
+      Row(
+        children: [
+              Image(image: AssetImage('images/pemuda.jpg'), width: 180,),
+              Wrap(
+                direction: Axis.horizontal,
+                children: [
+                  Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                ],
+              )
+        ],
       ),
       Icon(
         Icons.camera,
