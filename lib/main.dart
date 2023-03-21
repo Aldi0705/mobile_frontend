9c37cab56ecd159e2f0da1462fae989163c8abf6
aldi_syahputra <66547272+Aldi0705@users.noreply.github.com>
2023-03-21 15:08:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/home.dart';
+import 'package:myapp/pages/login/login.dart';
 import 'package:myapp/providers/news_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -18,12 +19,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home()
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Login());
   }
 }
